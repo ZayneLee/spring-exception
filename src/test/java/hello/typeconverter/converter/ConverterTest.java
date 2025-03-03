00f3e46cfe17dd440142e721fb9407edd6985032
ZayneLee <star1582@naver.com>
2025-03-03 15:36:00 +0900

    사용자 정의 타입 컨버터

--- src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -7,7 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import hello.exception.ExceptionApplication;
 import hello.exception.typeconverter.converter.IntegerToStringConverter;
+import hello.exception.typeconverter.converter.IpPortToStringConverter;
 import hello.exception.typeconverter.converter.StringToIntegerConveter;
+import hello.exception.typeconverter.converter.StringToIpPortConveter;
+import hello.exception.typeconverter.type.IpPort;
 
 @SpringBootTest(classes = ExceptionApplication.class)
 class ConverterTest {
@@ -24,5 +27,21 @@ class ConverterTest {
         IntegerToStringConverter converter = new IntegerToStringConverter();
         String result = converter.convert(10);
         assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void stringToIpPort() {
+        StringToIpPortConveter converter = new StringToIpPortConveter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+
+    @Test
+    void ipPortToString() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(ipPort);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
     }
 }
